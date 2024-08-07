2d3baa5f23552cc7d789df565a045c0a65470144
SUJU1998 <111004060+SUJU1998@users.noreply.github.com>
2024-08-07 15:32:35 +0530

    update

--- Backend/config/db.config.js
+++ Backend/config/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const ConnectionToDB=mongoose.connect(process.env.DB_URL);
+const ConnectionToDB=mongoose.connect(process.env.DB_URL,{serverSelectionTimeoutMS: 30000});
 
 module.exports={ConnectionToDB}
